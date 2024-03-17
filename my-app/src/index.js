9346947d501bf89697ef4ee83ceaf0121de83f49
Zhanna Yeloshenko <eloshenko.zhanna@gmail.com>
2024-03-17 18:14:48 +0300

    stage 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import App from './App'; // Assuming App is your root component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './index.scss';
+import 'macro-css';
+
+
+// Replace ReactDOM.render with createRoot
+const root = createRoot(document.getElementById('root'));
+
+// Render your app using createRoot
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
